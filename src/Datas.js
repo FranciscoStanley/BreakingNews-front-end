e2ffcae15172700c3115f3ee4fcccae0d7bbb7ee
Francisco stanley <franciscostanleyb@gmail.com>
2023-08-14 22:41:02 -0300

    feat(front-end): add data mocks

---
+++ src/Datas.js
@@ -0,0 +1,42 @@
+export const news = [
+  {
+    title: "News mocks 1",
+    text: "Lorem ipsum mock data fakes",
+    banner:
+      "https://imgs.search.brave.com/n27xwdBOTpcutmtlvP3ORu_TSYX6Y9FOHk8fT3xNlhU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9waWNz/dW0ucGhvdG9zLzgw/MC82MDA_cmFuZG9t/PTEw",
+    likes: 2,
+    comments: 4,
+  },
+  {
+    title: "News mocks 2",
+    text: "Lorem ipsum mock data fakes",
+    banner:
+      "https://imgs.search.brave.com/n27xwdBOTpcutmtlvP3ORu_TSYX6Y9FOHk8fT3xNlhU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9waWNz/dW0ucGhvdG9zLzgw/MC82MDA_cmFuZG9t/PTEw",
+    likes: 2,
+    comments: 4,
+  },
+  {
+    title: "News mocks 3",
+    text: "Lorem ipsum mock data fakes",
+    banner:
+      "https://imgs.search.brave.com/n27xwdBOTpcutmtlvP3ORu_TSYX6Y9FOHk8fT3xNlhU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9waWNz/dW0ucGhvdG9zLzgw/MC82MDA_cmFuZG9t/PTEw",
+    likes: 2,
+    comments: 4,
+  },
+  {
+    title: "News mocks 4",
+    text: "Lorem ipsum mock data fakes",
+    banner:
+      "https://imgs.search.brave.com/n27xwdBOTpcutmtlvP3ORu_TSYX6Y9FOHk8fT3xNlhU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9waWNz/dW0ucGhvdG9zLzgw/MC82MDA_cmFuZG9t/PTEw",
+    likes: 2,
+    comments: 4,
+  },
+  {
+    title: "News mocks 5",
+    text: "Lorem ipsum mock data fakes",
+    banner:
+      "https://imgs.search.brave.com/n27xwdBOTpcutmtlvP3ORu_TSYX6Y9FOHk8fT3xNlhU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9waWNz/dW0ucGhvdG9zLzgw/MC82MDA_cmFuZG9t/PTEw",
+    likes: 2,
+    comments: 4,
+  },
+];
